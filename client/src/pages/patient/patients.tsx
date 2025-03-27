265e76321d4723d53787e52c76eabdc93645f801
blakedrake37 <142980009+blakedrake37@users.noreply.github.com>
2025-03-27 14:34:27 +0700

    Patients page

--- client/src/pages/patient/patients.tsx
+++ client/src/pages/patient/patients.tsx
@@ -86,7 +86,7 @@ export const PatientsPage: React.FC = () => {
       <div className="relative mb-6">
         <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 h-4 w-4" />
         <Input
-          placeholder="Search patients by name, owner, or species..."
+          placeholder="Search patients..."
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
           className="pl-10"
